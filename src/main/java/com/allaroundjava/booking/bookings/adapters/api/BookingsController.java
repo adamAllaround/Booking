62ec33e9c3b37e58810d75e0e96b005663b9c67b
adamAllaround <adam@allaroundjava.com>
2021-06-05 18:14:37 +0200

    Add Bookings Controller

--- src/main/java/com/allaroundjava/booking/bookings/adapters/api/BookingsController.java
+++ src/main/java/com/allaroundjava/booking/bookings/adapters/api/BookingsController.java
@@ -2,22 +2,35 @@ package com.allaroundjava.booking.bookings.adapters.api;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/items")
 @AllArgsConstructor
 class BookingsController {
-//    @GetMapping("/{itemId}/bookings")
-//    ResponseEntity<BookingsResponse> getBookings() {
-//
-//    }
-//
-//    @PostMapping("/{itemId}/bookings")
-//    ResponseEntity<BookingRequest> addBooking() {
-//
-//    }
+    private final BookingsFacade bookingsFacade;
+    @GetMapping("/{itemId}/bookings")
+    ResponseEntity<BookingsResponse> getBookings(@PathVariable UUID itemId) {
+        return ResponseEntity.ok(bookingsFacade.getAllByItemId(itemId));
+    }
+
+    @PostMapping("/{itemId}/bookings")
+    ResponseEntity<BookingResponse> addBooking(@PathVariable UUID itemId, @RequestBody BookingRequest request) {
+        Optional<BookingResponse> result = bookingsFacade.save(itemId, request);
+        return result.map(resp -> ResponseEntity.created(getUri(resp)).body(resp))
+                .orElseGet(() -> ResponseEntity.badRequest().build());
+    }
+
+    private URI getUri(BookingResponse resp) {
+        return ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(resp.getId())
+                .toUri();
+    }
 }
