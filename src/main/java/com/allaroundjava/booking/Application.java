b6687c07e45aeaea98ad304c18bec7e90c17100f
adamAllaround <adam@allaroundjava.com>
2021-06-16 08:07:37 +0200

    Fix spring config

--- src/main/java/com/allaroundjava/booking/Application.java
+++ src/main/java/com/allaroundjava/booking/Application.java
@@ -1,13 +1,19 @@
 package com.allaroundjava.booking;
 
+import com.allaroundjava.booking.bookings.config.BookingsConfig;
+import com.allaroundjava.booking.common.DatabaseConfig;
+import com.allaroundjava.booking.common.events.EventsConfig;
+import com.allaroundjava.booking.items.ItemsConfig;
+import com.allaroundjava.booking.owners.OwnersConfig;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication(scanBasePackages = {"com.allaroundjava.booking.owners", "com.allaroundjava.booking.items",
-"com.allaroundjava.booking.common", "com.allaroundjava.booking.bookings"},
-        exclude = {HibernateJpaAutoConfiguration.class})
+@SpringBootConfiguration
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@Import({DatabaseConfig.class, EventsConfig.class, OwnersConfig.class, ItemsConfig.class, BookingsConfig.class})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
